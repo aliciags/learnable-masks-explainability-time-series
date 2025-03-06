344a5a7bfba8eba21bf1ebe9d84e9a5ba2072cb2
aliciags <aliciags99@gmail.com>
2025-03-06 13:37:23 +0100

    refactor

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,6 @@ from physioex.physioex.preprocess.sleepedf import SLEEPEDFPreprocessor
 from physioex.physioex.preprocess.utils.signal import  xsleepnet_preprocessing
 
 # Initialize Preprocessor 
-# I believe the shape is not needed, already defined in the class by default, only the folder data
-# By default 3 channels not 4
 preprocessor = SLEEPEDFPreprocessor(
     preprocessors_name = ["xsleepnet"], # the name of the preprocessor
     preprocessors = [xsleepnet_preprocessing], # the callable preprocessing method
