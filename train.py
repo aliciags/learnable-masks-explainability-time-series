70f58a6433fc342840200f8114bc6a2c1a9f4565
aliciags <aliciags99@gmail.com>
2025-03-14 18:58:07 +0100

    model training

--- train.py
+++ train.py
@@ -5,43 +5,34 @@ import importlib
 
 from physioex.train.utils import train, test
 from physioex.data import PhysioExDataModule
+from physioex.train.models.load import load_model
+
+
+target_package = "physioex.train.networks.utils.target_transform"
+target_class = "get_mid_label"
+target = getattr(importlib.import_module(target_package), target_class)
 
 datamodule = PhysioExDataModule(
     datasets=["sleepedf"],     # list of datasets to be used
-    batch_size=64,             # batch size for the DataLoader
+    batch_size=128,            # batch size for the DataLoader
     preprocessing="raw",       # preprocessing method
     selected_channels=["EEG"], # channels to be selected
-    sequence_length=21,        # length of the sequence
+    sequence_length=19,         # length of the sequence
+    target_transform= target,  # since seq to epoch, target seq
+    num_workers = 10,          # number of parallel workers
     data_folder = "/work3/s241931/data/"    # path to the data folder
 )
 
 checkpoint_path = "./model/checkpoint/"
 
-# with open("./physioex/physioex/train/networks/config/chambon2018.yaml", "r") as file:
 with open("./config.yaml", "r") as file:
     config = yaml.safe_load(file)
 
 network_config = config["model_config"]
 
-# load the loss function 
-loss_package, loss_class = network_config["loss"].split(":")
-model_loss = getattr(importlib.import_module(loss_package), loss_class)
-
-# in case you provide model_name the system loads the additional model parameters from the library
-if "model_name" in config:
-    model_name = config["model_name"]
-
 # load the model class
 model_package, model_class = config["model"].split(":")
 model_class = getattr(importlib.import_module(model_package), model_class)
-
-datamodule_kwargs = {
-    "selected_channels" : ["EEG"], # needs to match in_channels
-    "sequence_length" : int(network_config["sequence_length"]),
-    "target_transform" : config["target_transform"],
-    "preprocessing" : config["input_transform"],
-    "data_folder" : "/work3/s241931/data/",
-}
 
 # casting the float types
 network_config['learning_rate'] = float(network_config['learning_rate'])
@@ -54,22 +45,24 @@ network_config['adam_epsilon'] = float(network_config['adam_epsilon'])
 # Train the model
 best_checkpoint = train(
     datasets = datamodule,
-    datamodule_kwargs = datamodule_kwargs,
     model_class = model_class,
     model_config = network_config,
     checkpoint_path = checkpoint_path,
-    batch_size = 64,
-    max_epochs = 10
+    batch_size = 128,
+    max_epochs = 10,
+    resume = False
 )
+
+# best_checkpoint = "fold=-1-epoch=9-step=20214-val_acc=0.82.ckpt"
 
 # Test the model
 results_dataframe = test(
     datasets = datamodule,
-    datamodule_kwargs = datamodule_kwargs,
     model_class = model_class,
     model_config = network_config,
-    chekcpoint_path = os.path.join( checkpoint_path, best_checkpoint ),
-    batch_size = 64,
-    results_dir = checkpoint_path,  # if you want to save the test results 
-                                    # in your checkpoint directory
+    checkpoint_path = os.path.join( checkpoint_path, best_checkpoint ),
+    batch_size = 128,
+    results_path = checkpoint_path,
 )
+
+print(results_dataframe.head())
--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #BSUB -q gpuv100
 #BSUB -J train                   # Job name
-#BSUB -n 4                       # Number of cores
+#BSUB -n 12                      # Number of cores
 #BSUB -W 24:00                   # Wall-clock time (24 hours here)
 #BSUB -R "rusage[mem=8GB]"       # Memory requirements (8 GB here)
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -o train_%J.out            # Standard output file
-#BSUB -e train_%J.err            # Standard error file
+#BSUB -o train19_%J.out            # Standard output file
+#BSUB -e train19_%J.err            # Standard error file
 
 # Activate the environment
 source /zhome/2b/8/212341/learnable-masks-explainability-time-series/.env/bin/activate
