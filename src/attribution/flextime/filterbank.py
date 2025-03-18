90b375eb3870ea850420f83d7449546a41a5dcb5
aliciags <aliciags99@gmail.com>
2025-03-18 10:27:33 +0100

    apply filter and docu

--- src/attribution/flextime/filterbank.py
+++ src/attribution/flextime/filterbank.py
@@ -22,7 +22,13 @@ class Filterbank:
         else:
             self.bandwidth = bandwidth
 
+        # create filterbank
+        self.create_filterbank()
+
     def create_filterbank(self):
+        """
+        Create filterbank with n_filters filters.
+        """
         # first filter pass low
         h = signal.firwin(self.n_taps, self.bandwidth, fs=self.sample_freq, pass_zero="lowpass")
         self.bank.append(h)
@@ -39,9 +45,15 @@ class Filterbank:
         self.bank.append(h)
 
     def get_filterbank(self):
+        """
+        Get the filterbank.
+        """
         return self.bank
     
     def plot_filterbank(self):
+        """
+        Plot the filterbank.
+        """
         # plot filterbank in a single plot
         plt.figure()
         cmap = get_cmap('hsv', len(self.bank))
@@ -52,4 +64,59 @@ class Filterbank:
         plt.xlabel("Frequency (Hz)")
         plt.ylabel("Gain")
         plt.show()
+
+    def apply_filterbank(self, data, time_axis: int =-1):
+        """
+        Apply filterbank to data.
         
+        Parameters
+        ----------
+        data : np.ndarray
+            Input data with shape (N, T) where N is number of sequences, T is number of time points.
+        time_axis : int
+            The time axis of the data.
+
+        Returns
+        -------
+        np.ndarray
+            The output of the filterbank.
+        """
+        # apply filterbank to data
+        y = np.zeros((*data.shape, self.n_filters))
+        for i, h in enumerate(self.bank):
+            y[..., i] = signal.lfilter(h, 1, data, axis=time_axis)
+        return y 
+    
+    def forward(self, data, mask = None, time_axis: int =-1):
+        """
+        Apply filterbank to data and apply mask if provided.
+
+        Parameters
+        ----------
+        data : np.ndarray
+            Not 100% sure about the shape of the data.
+            Input data with shape (N, C, T) where N is number of samples, C is number of channels, T is number of time points.
+        mask : np.ndarray
+            Mask with shape (N, T) or (T, ) to apply to the data.
+        time_axis : int
+            The time axis of the data.
+
+        Returns
+        -------
+        np.ndarray
+            The output of the filterbank with mask applied summed over the filter dimension.
+        """
+        # apply filterbank to data
+        y = self.apply_filterbank(data, time_axis)
+
+        # apply mask if provided
+        if mask is not None:
+            # if the mask dimension is 1
+            if len(mask.shape) == 1:
+                mask = mask[np.newaxis, :]  # convert (N, ) to (1, N)
+                y = y * mask
+            # if the mask dimension is 2
+            else:
+                y = y * mask[:, np.newaxis, :]
+
+        return np.sum(y, axis=-1)
