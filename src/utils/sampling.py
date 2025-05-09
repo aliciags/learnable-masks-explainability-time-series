9115448d4cb16e293540133ad67c2cb63bb6204a
aliciags <aliciags99@gmail.com>
2025-05-09 15:11:46 +0200

    resampling wavedec

---
+++ src/utils/sampling.py
@@ -0,0 +1,32 @@
+import math
+import numpy as np
+
+def upsampling_wavedec(coeffs):
+    """
+    Upsample the coefficients of the multilevel wavelet transform 
+    to match the original signal length.
+    """
+    length = len(coeffs[-1]) * 2
+
+    upsampled_coeffs = []
+    for coeff in coeffs:
+        factor = math.ceil(length/len(coeff))
+
+        # Upsample coefficients to match the original signal length
+        upsampled = np.repeat(coeff, factor)[:length]
+        
+        upsampled_coeffs.append(upsampled)
+    return np.array(upsampled_coeffs)
+
+def downsample_wavedec(coeffs):
+    """
+    Downsample the coefficients of the wavelet transform to match the original signal length.
+    """
+    coeffs = coeffs[::-1]
+
+    downsampled_coeffs = []
+    for i, coeff in enumerate(coeffs):
+        c = coeff[1::(2**(i+1))]
+
+        downsampled_coeffs.append(c)
+    return downsampled_coeffs[::-1]
